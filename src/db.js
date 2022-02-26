cb96a9077c18c8f7e54677b960156a9e3d86d572
Amanda <amanda@Amandas-MBP-8.cable.rcn.com>
2022-02-26 14:53:45 -0500

    updated db file

--- src/db.js
+++ src/db.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose');
+const User = new Schema({
+    username: String,
+    password: String,
+});
+const Internship = new mongoose.Schema({
+    user: String,
+    company: String,
+    role: String,
+    status: String,
+    date: String
+})
+mongoose.model('User', User);
+mongoose.model('Internship', Internship);
+const uri = process.env.MONGODB_URI || 'mongodb://localhost/buzzZ';
+mongoose.connect(uri,{useNewUrlParser: true});
+module.exports = {mongoose};
